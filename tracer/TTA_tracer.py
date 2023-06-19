a0b6bb03f201a6157789e1f991a56ad2ea8f4e75
G0lge <yusufcan_31@hotmail.com>
2023-06-19 05:57:24 +0300

    prints taint to flags

--- tracer/TTA_tracer.py
+++ tracer/TTA_tracer.py
@@ -10,6 +10,8 @@ import ida_ua
 import ida_nalt
 import triton
 from triton import *
+from keystone import *
+
 
 
 from tracer.ui2 import tracer_read_dialog, tracer_read_size_dialog, tracer_write_data_dialog, tracer_change_register, tracer_context_menu_dialog, tracer_taint_mem_dialog, tracer_taint_mem_size_dialog, tracer_taint_menu_dialog
@@ -30,7 +32,7 @@ class TritonTracer():
 	insn = ida_ua.insn_t()
 	function = {}
 	pushval = 0
-	block = BasicBlock([Instruction(b"\x90"),Instruction(b"\x90"),Instruction(b"\x90"),Instruction(b"\x90"),Instruction(b"\x90"),Instruction(b"\x90"),Instruction(b"\x68\x11\xCA\x00\x00")])
+	block = BasicBlock([Instruction(b"\x90"),Instruction(b"\x90"),Instruction(b"\x90"),Instruction(b"\x90"),Instruction(b"\x90"),Instruction(b"\x90")])
 	symb_ex_end = 0
 	patch_start = 0
 	registers = [] 
@@ -40,6 +42,11 @@ class TritonTracer():
 	dontrun = False
 	ip = 0
 	retard_registers = []
+	registers_oldval = []
+	add_later = False
+	sub_later = False
+	retard_opt_lol = False
+	ks = Ks(KS_ARCH_X86, KS_MODE_64)  # Initialize Keystone for x86-64
 
 	def loadBinary(self):
 		last_addr = idaapi.inf_get_max_ea()
@@ -77,35 +84,90 @@ class TritonTracer():
 		self.Triton.registers.sf,
 		self.Triton.registers.zf,
 		self.Triton.registers.df ]
-		self.Triton.taintRegister(self.Triton.registers.ecx)
-		self.Triton.taintRegister(self.Triton.registers.edx)
-		#self.Triton.taintRegister(self.Triton.registers.rsp)
+
+		self.registers_oldval = {register_id.getType(): 0 for register_id in self.Triton.getAllRegisters()}
+
+		self.Triton.taintRegister(self.Triton.registers.rax)
+		self.Triton.taintRegister(self.Triton.registers.rcx)
+		self.Triton.taintRegister(self.Triton.registers.rdx)
+		self.Triton.taintRegister(self.Triton.registers.rbx)
+		self.Triton.taintRegister(self.Triton.registers.rsp)
+		self.Triton.taintRegister(self.Triton.registers.rbp)
+		self.Triton.taintRegister(self.Triton.registers.rsi)
+		self.Triton.taintRegister(self.Triton.registers.rdi)
+
+		self.Triton.taintRegister(self.Triton.registers.r8)
+		self.Triton.taintRegister(self.Triton.registers.r9)
+		self.Triton.taintRegister(self.Triton.registers.r10)
+		self.Triton.taintRegister(self.Triton.registers.r11)
+		self.Triton.taintRegister(self.Triton.registers.r12)
+		self.Triton.taintRegister(self.Triton.registers.r13)
+		self.Triton.taintRegister(self.Triton.registers.r14)
+		self.Triton.taintRegister(self.Triton.registers.r15)
+
+		self.Triton.taintRegister(self.Triton.registers.cf)
+		self.Triton.taintRegister(self.Triton.registers.sf)
+		self.Triton.taintRegister(self.Triton.registers.zf)
+		self.Triton.taintRegister(self.Triton.registers.df)
 
 	
 
 	def log_read_write(self,ea):
 		this_inst = self.inst
-		if  (not self.inst.isMemoryRead() and not self.inst.isMemoryWrite()) or this_inst.getType() == OPCODE.X86.POP or this_inst.getType() == OPCODE.X86.PUSH or this_inst.getType() == OPCODE.X86.PUSHF or this_inst.getType() == OPCODE.X86.POPF :
+		print("logwriteinit")
+		if  (not self.inst.isMemoryRead() and not self.inst.isMemoryWrite()):
 			return
+
+		print("logwrite0")
+
+		if this_inst.getType() == OPCODE.X86.POP or this_inst.getType() == OPCODE.X86.PUSH or this_inst.getType() == OPCODE.X86.PUSHF or this_inst.getType() == OPCODE.X86.POPF:
+			return
+
+		print("logwrite1")
 
 
 		operands = self.inst.getOperands()
 
 		if len(operands) <= 1:
 			return
+		print("logwrite2")
 			
 		if operands[0].getType() == OPERAND.MEM:
-			return
-			if self.Triton.isMemoryTainted(value = self.Triton.getConcreteMemoryValue( MemoryAccess( operands[0], operands[0].getSize() ) )):
-				return
+			opaddr = operands[0].getAddress()
+			opsize = operands[0].getSize()
+			if operands[1].getType() == OPERAND.REG:
+				if self.Triton.isRegisterTainted(operands[1]):
+					print("tainted:", hex(opaddr) )
+					self.Triton.taintMemory(operands[0])
+					self.Triton.taintMemory( MemoryAccess( opaddr, opsize ))
 
-		if len(operands) <= 2:
+				else:
+					print("untainted:", hex(opaddr) )
+					self.Triton.untaintMemory(operands[0])
+					self.Triton.untaintMemory( MemoryAccess( opaddr, opsize ))
+
+			if self.Triton.isMemoryTainted(  operands[0] ):
+				return
 			return
+
+		print("logwrite3")
+
+
+		print("logwrite4")
+
+
 
 		if operands[1].getType() == OPERAND.MEM:
-			if self.Triton.isMemoryTainted(value = self.Triton.getConcreteMemoryValue( MemoryAccess( operands[1], operands[1].getSize() ) )):
+			operand_addr = operands[1].getAddress()
+			print("operand addr:",operand_addr)
+			istaint_addr = self.Triton.isMemoryTainted( operands[1]  )
+			print("tainted?:", istaint_addr, "tainted2?",self.Triton.isMemoryTainted( MemoryAccess(operand_addr, operands[1].getSize() )  ) )
+			if istaint_addr:
 				print("memory tainted")
 				return
+		print("logwrite5")
+
+		newval = self.Triton.getConcreteMemoryValue( MemoryAccess( operands[1].getAddress(), operands[1].getSize() ) )
 		
 		
 		self.constPropagation(operands[0],newval,ea)
@@ -202,6 +264,17 @@ class TritonTracer():
 			self.peepholeRegister = operand
 
 
+	def createweirdmov_(self,registername,imm):
+		instruction = f"mov {registername}, {imm}"
+		encoding, count = self.ks.asm(instruction)
+
+		# Print the assembled instruction bytes
+		rbytes = b""
+		for byte in encoding:
+			rbytes = rbytes + (byte.to_bytes(1,"little"))
+
+		return rbytes
+
 
 	def peepholeOpt(self,ea):
 
@@ -221,24 +294,110 @@ class TritonTracer():
 				return
 		else:
 			return
+
+
 		if len(operands) == 2:
 			if operands[1].getType() == OPERAND.REG:
 				if self.Triton.isRegisterTainted(operands[1]):
 					print("second register tainted")
 					return
+			else:
+				return
+
+		oldval_1 = self.registers_oldval[operands[0].getType()]
+		#oldval_2 = self.Triton.getConcreteRegisterValue(operands[1])
 
 		self.ip = self.Triton.getRegisterAst(self.Triton.registers.rip).evaluate()
+
+		newval_1 = self.Triton.getConcreteRegisterValue(operands[0])
+		#newval_2 = self.Triton.getConcreteRegisterValue(operands[1])
+
+		if (oldval_1 == newval_1): #or (oldval_2 == newval_2):
+			print("val didnt change oldval:",oldval_1, "newval:", newval_1)
+			return
+
 		self.dontrun = True
 		print(self.inst)
-		newval = self.Triton.getConcreteRegisterValue(operands[0])
+		
 
 
-		self.constPropagation(operands[0],newval,ea)
-		print("peepholing",operands[0], newval)
+		self.constPropagation(operands[0],newval_1,ea)
+
+		if this_inst.getType() == OPCODE.X86.XOR or this_inst.getType() == OPCODE.X86.AND or this_inst.getType() == OPCODE.X86.OR:
+			if operands[1].getType() == OPERAND.REG and not self.Triton.isRegisterTainted(operands[1]): 
+				self.block.add(Instruction( self.createweirdmov_(operands[1].getName(), self.Triton.getConcreteRegisterValue(operands[1]) ) )) # sub rsp, regsize
+				return
+
+
+		print("peepholing",operands[0], newval_1)
+		return
+
+
+	def createweirdmovrsp(self,registername):
+		instruction = f"mov [rsp], {registername}"
+		encoding, count = self.ks.asm(instruction)
+
+		# Print the assembled instruction bytes
+		rbytes = b""
+		for byte in encoding:
+			rbytes = rbytes + (byte.to_bytes(1,"little"))
+
+		return rbytes
+
+	def createweirdmovfromrsp(self,registername):
+		instruction = f"mov {registername}, [rsp]"
+		encoding, count = self.ks.asm(instruction)
+
+		# Print the assembled instruction bytes
+		rbytes = b""
+		for byte in encoding:
+			rbytes = rbytes + (byte.to_bytes(1,"little"))
+
+		return rbytes
+
+	def poppushOpt(self,ea):
+
+		this_inst = self.inst
+		operands = this_inst.getOperands()
+
+		if this_inst.getType() == OPCODE.X86.PUSHFQ:
+			self.dontrun = True
+			self.function[ea] = b"\x68\x00\x00\x00\x00" # push 0
+			return
+
+		if not (this_inst.getType() == OPCODE.X86.POP or this_inst.getType() == OPCODE.X86.PUSH):
+			return
+
+		
+
+		if operands[0].getType() == OPERAND.REG:
+			if self.Triton.isRegisterTainted(operands[0]):
+				print("register tainted")
+				#return
+		else:
+			return
+		
+
+		self.ip = self.Triton.getRegisterAst(self.Triton.registers.rip).evaluate()
+		
+		print(self.inst)
+
+		self.retard_opt_lol = True
+
+		if this_inst.getType() == OPCODE.X86.PUSH:
+			self.block.add(Instruction(b"\x48\x83\xEC"+operands[0].getSize().to_bytes(1,"little"))) # sub rsp, regsize
+			self.function[ea] = self.createweirdmovrsp(operands[0].getName())
+			self.dontrun = True
+			return
+
+		if this_inst.getType() == OPCODE.X86.POP:
+			self.block.add(Instruction(self.createweirdmovfromrsp(operands[0].getName()) ) )  
+			self.function[ea] = b"\x48\x83\xC4"+operands[0].getSize().to_bytes(1,"little") # add rsp, regsize
+			self.dontrun = True
+			return
+
+
 		return 
-
-
-
 
 
 
@@ -262,15 +421,60 @@ class TritonTracer():
 
 			print ( "Removing:", block_len - 1 ,self.block.remove(block_len - 1 ) )
 
+			if self.retard_opt_lol:
+
+				print ( "Removing2:", block_len - 2 ,self.block.remove(block_len - 2 ) )
+				self.retard_opt_lol = False
+
+		self.poppushOpt(ea)
 		self.log_read_write(ea)
 		self.peepholeOpt(ea)
+		
 
+		#####################
+		# if any is tainted:
+		#
+		#  push rax
+		#  xor [rsp], rcx
+		#  pop rax
+		#
+		# to this:
+		#
+		#  sub rsp, regsize
+		#  mov [rsp], rax
+		#
+		#  xor [rsp], rcx
+		#
+		#  mov rax, [rsp]
+		#  add rsp, regsize
+		######################
+		# if rax and rcx not tainted:
+		#
+		#  push rax
+		#  xor [rsp], rcx
+		#  pop rax
+		#
+		# to this:
+		#
+		#  sub rsp, regsize
+		#  mov [rsp], rax
+		#
+		#  add rsp, regsize
+		#  push raxval ^ rcx
+		#  mov rcx, raxval ^ rcx
+		#
+		#  mov rax, [rsp]
+		#  add rsp, regsize
+		#######################
 
 
 
 		if  not ( self.inst.getDisassembly()[0] == "j"  or "ret" in self.inst.getDisassembly() or  "call" in self.inst.getDisassembly() )   :
 
 			self.block.add(Instruction(self.function[ea]))
+
+		if self.inst.getType() == OPCODE.X86.CALL:
+			self.block.add(Instruction(b"\x48\x83\xEC\x08")) # sub rsp, 8
 
 		self.pushval = 0
 
@@ -288,6 +492,8 @@ class TritonTracer():
 
 		self.dontrun = False
 		#registers
+
+		self.registers_oldval = {register_id.getType(): self.Triton.getConcreteRegisterValue(register_id) for register_id in self.Triton.getAllRegisters()}
 
 		ax = self.Triton.getConcreteRegisterValue(self.Triton.registers.rax)
 
@@ -324,17 +530,27 @@ class TritonTracer():
 		#flags
 
 		cf = self.Triton.getConcreteRegisterValue(self.Triton.registers.cf)
+		cf_taint = self.Triton.isRegisterTainted(self.Triton.registers.cf)
 
 		sf = self.Triton.getConcreteRegisterValue(self.Triton.registers.sf)
+		sf_taint = self.Triton.isRegisterTainted(self.Triton.registers.sf)
 
 		zf = self.Triton.getConcreteRegisterValue(self.Triton.registers.zf)
+		zf_taint = self.Triton.isRegisterTainted(self.Triton.registers.zf)
 
 		df = self.Triton.getConcreteRegisterValue(self.Triton.registers.df)
+		df_taint = self.Triton.isRegisterTainted(self.Triton.registers.df)
+
+		of = self.Triton.getConcreteRegisterValue(self.Triton.registers.of)
+		of_taint = self.Triton.isRegisterTainted(self.Triton.registers.of)
+
+		af = self.Triton.getConcreteRegisterValue(self.Triton.registers.af)
+		af_taint = self.Triton.isRegisterTainted(self.Triton.registers.af)
 
 
 
-
-		print('Next ip:', hex(self.ip), "rax: ", hex(ax), "rcx:", hex(cx),"rdx:", hex(dx),"rbx:", hex(bx),"rsi:", hex(si),"rdi:", hex(di),"rsp:", hex(sp), "rbp:", hex(bp), "r8:",hex(r8),"r9:",hex(r9),"r10:",hex(r10),"r11:",hex(r11),"r12:",hex(r12),"r13:",hex(r13),"r14:",hex(r14),"r15:",hex(r15), "zf:",hex(zf),"sf:",hex(sf) ,"df:",hex(df),"cf:",hex(cf), )
+		print('Next ip:', hex(self.ip), "rax: ", hex(ax), "rcx:", hex(cx),"rdx:", hex(dx),"rbx:", hex(bx),"rsi:", hex(si),"rdi:", hex(di),"rsp:", hex(sp), "rbp:", hex(bp), "r8:",hex(r8),"r9:",hex(r9),"r10:",hex(r10),"r11:",hex(r11),"r12:",hex(r12),"r13:",hex(r13),"r14:",hex(r14),"r15:",hex(r15) ) 
+		print("Flags:","zf:",hex(zf), zf_taint  ,"sf:",hex(sf), sf_taint ,"df:",hex(df), df_taint,"cf:",hex(cf),cf_taint,"of:",hex(of),of_taint,"cf:",hex(af),af_taint, )
 		
 		return self.ip
 
@@ -406,7 +622,7 @@ class TritonTracer():
 			return self.Triton.untaintMemory( taintval )
 
 
-		value = self.Triton.taintMemory( taintval )
+		return self.Triton.taintMemory( taintval )
 
 
 	def tracer_taint_menu_dialog(self):
@@ -414,10 +630,10 @@ class TritonTracer():
 		a.show()
 
 	def taint_register(self, register):
-		if self.Triton.isMemoryTainted(register):
+		if self.Triton.isRegisterTainted(register):
 			return self.Triton.untaintRegister( register )
-		self.Triton.taintRegister(register)
-		return
+		return self.Triton.taintRegister(register)
+		
 
 	def change_register_dialog(self, register):
 		write_memory_address = tracer_change_register(register.getName())
